5eaa30957ee9099b8a9996a83944d208f8b55078
boringcat <c654477757@gmail.com>
2026-03-23 16:50:47 +0800

    函数化处理

--- server/ctx.go
+++ server/ctx.go
@@ -1,5 +1,10 @@
 package server
 
+import (
+	"context"
+	"net/url"
+)
+
 type CtxKey struct {
 	name string
 }
@@ -9,3 +14,26 @@ var (
 	CacheKey    = CtxKey{"CacheKey"}
 	RequestId   = CtxKey{"RequestId"}
 )
+
+func getUpStream(ctx context.Context) (uri *url.URL, ok bool) {
+	uri, ok = ctx.Value(UpStreamURL).(*url.URL)
+	return
+}
+func getCacheKey(ctx context.Context) (key string, ok bool) {
+	key, ok = ctx.Value(CacheKey).(string)
+	return
+}
+func getRequestId(ctx context.Context) (id string, ok bool) {
+	id, ok = ctx.Value(RequestId).(string)
+	return
+}
+
+func setUpStream(ctx context.Context, uri *url.URL) context.Context {
+	return context.WithValue(ctx, UpStreamURL, uri)
+}
+func setCacheKey(ctx context.Context, key string) context.Context {
+	return context.WithValue(ctx, CacheKey, key)
+}
+func setRequestId(ctx context.Context, id string) context.Context {
+	return context.WithValue(ctx, RequestId, id)
+}
